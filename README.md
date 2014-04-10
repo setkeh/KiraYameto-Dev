f8747ea4ab52bb73335d1013e85653bfd2057511
setkeh <setkeh@gmail.com>
2014-04-10 23:51:24 +1000

    Added Current Code

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ KiraYameto-Dev
 ==============
 
 KiraYameto Irc Bot Dev
+
+This Repo Contains All the Dev Files for KiraYameto main bot is bot.js all other files are debug and test objects
+Kira is Powered by Node.js and V8.
---
+++ Test.js
@@ -0,0 +1,13 @@
+var http = require('http')
+
+var options = {
+  host: 'www.google.com',
+  port: 80,
+  path: '/index.html'
+};
+
+http.get(options, function(res) {
+  console.log("Got response: " + res.statusCode);
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
+});
