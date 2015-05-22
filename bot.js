99213e9fac3901b0664aa30597a8a665062753ea
Setkeh <setkeh@gmail.com>
2015-05-22 14:34:22 +1000

    Added Docker Support Removed some Functionality for #LDC

--- bot.js
+++ bot.js
@@ -1,80 +1,167 @@
+//Import the Libs
+var IRC = require('internet-relay-chat');
+var request = require("request");
+var os = require("os");
+var fs =  require("fs");
+var github = require('octonode');
+var gith = require('gith').create( 9001 );
+var Docker = require('dockerode');
+var docker = new Docker({host: 'http://192.168.1.120', port: 4243});
+
 // Create the configuration
 var config = {
 channels: ["#thesetkehproject", "#linuxdistrocommunity"],
 server: "irc.freenode.net",
-botName: "KiraYameto",
-userName: "KiraYameto",
+port: "7000",
+username: "KiraYameto",
+nick: "KiraYameto",
 password: "PASSWORD",
+autoReconnect: 150,
+floodDelay: 1000,
 secure: true,
 autoRejoin: true,
-autoConnect: true,
-realName: "LDC ADMIN BOT"
+debug: true,
+vhost: null,
+realname: "LDC ADMIN BOT",
+trigger: "!",
+machine: "RaspberryPi", // This is Machine type for example "Dell Poweredge 2650"
+maintainer: "SETKEH" // Yourname Here.
 };
 
-// Get the libs
-var irc = require("irc");
-var request = require("request");
-var os = require("os");
-var fs =  require("fs"); //Used for Parsing Large Json for output purposes and logging.
-var github = require('octonode');
-
-// Create Global Config Variables
-var trigger = "!";
-var machine = "RaspberryPi"; // This is Machine type for example "Dell Poweredge 2650"
-var maintainer = "SETKEH"; // Yourname Here.
-
-// Create the bot name
-var bot = new irc.Client(config.server, config.botName, {
-    autoConnect: config.autoConnect,
-    channels: config.channels,
-    userName: config.userName,
-    realName: config.realName,
-    autoRejoin: config.autoRejoin,
-    password: config.password
-  });
-
-// Start Monitoring for Changes to code
-/*
-fs.watch(".", { persistent: true }, function (event, fileName) {
-  console.log("Event: " + event);
-  console.log(fileName + "\n");
+//Create the bot instance.
+var bot = new IRC({
+server: config.server,
+username: config.username,
+nick: config.nick,
+password: config.password,
+realname: config.realname,
+port: config.port,
+autoConnect: config.autoConnect,
+floodDelay: config.floodDelay,
+secure: config.secure,
+autoRejoin: config.autoRejoin,
+vhost: config.vhost,
+debug: config.debug
 });
-*/
 
-// Use for Dbugging Non Joins.
-//console.log(bot);
-//console.log(err);
+//Inform the Console we're connected.
+bot.on('connect', function() {
+    console.log('Bot connected');
+});
 
-//Log Errors instead of Crashing "Hopefully" TM
-bot.addListener('error', function(message) {
-  console.log('error: ', message);
+//Start the bot
+bot.connect();
+
+//Once Reg'ed with the Server (Handshake wit IRCD)
+bot.on('registered', function() {
+    bot.join(config.channels[0]);
+    bot.join(config.channels[1]);
+});
+
+//Start Docker API intergration
+bot.on('message', function(sender, channel, message) {
+  if (message == config.trigger + "listcontainers") {
+    docker.listContainers({all: true}, function(err, containers) {
+      for(var i = 0; i < containers.length; i++) {
+        bot.message(channel, IRC.colors.cyan + "Name: " + IRC.colors.black + containers[i].Names + IRC.colors.cyan + " Status: " + IRC.colors.black + containers[i].Status);
+      }
+    });
+  }
+  if (message == config.trigger + "listimages") {
+    docker.listImages({all: false}, function(err, images) {
+      for(var i = 0; i < images.length; i++) {
+        bot.message(channel, images[i].RepoTags + IRC.colors.cyan + " Size: " + IRC.colors.black + (images[0].VirtualSize/1024/1024).toPrecision(4) + "MB");
+      }
+    });
+  }
+  if (message == config.trigger + "dockerver") {
+    docker.version(function(err, ver) {
+        bot.message(channel, IRC.colors.cyan + "Version: " + IRC.colors.black + ver.Version);
+      });
+  }
+});
+
+//Start URL Scraping
+bot.on('message', function(sender, channel, message) {
+	var msgArray = message.split(" ");
+	var Chan = channel
+  if (Chan != "#linuxdistrocommunity") { // Now LDC has LDC-BOT we dont need to Scrap that Channel.
+	//console.log(message);
+    for (var i = 0; i < msgArray.length; i++) {
+
+      if (msgArray[i].match(".jpg") || msgArray[i].match(".png") || msgArray[i].match("imgur") || msgArray[i].match(".gif") || msgArray[i].match("McClane2")) {
+        console.log(Chan, "Ignoring Image");
+      } else {
+
+        if (msgArray[i].match("http://") || msgArray[i].match("https://")) {
+          var url = msgArray[i];
+
+          request({uri: url}, function(err, response, body) {
+
+            if (!err && response.statusCode) {
+              var regex_match = body.match(/<title>(.+?)<\/title>/)[1];
+              bot.message(Chan,  "[ Page Title: " + url + " " + regex_match + " ]");
+            } else {
+              bot.message(Chan, url + " Could not be found");
+              bot.message(Chan, err.message);
+            }
+
+          });
+        };
+      };
+    };
+  }
 });
 
 // Listen for joins
-bot.addListener("join", function(channel, who) {
+bot.on("join", function(user, channel) {
   // Welcome them in!
-  if ((who) != config.botName) {
+  if ((user.nick) != config.nick) {
     if ((channel) == "#thesetkehproject") {
-      bot.say(channel, who + " Duuuuuude Welcome to the" + channel + "!!");
+      bot.message(channel, user.nick + " Duuuuuude Welcome to the " + channel + "!!");
     };
   };
 });
 
-// Listen for any message, PM said user when he posts
-bot.addListener("pm", function(from, to, text, message) {
-  bot.say(from, "I am a Bot and my Brains Fell Out");
-});
-
-// Listen for any message, say to him/her in the room
-bot.addListener("message", function(from, to, text, message, channel) {
-  if (to == config.botName) {
-    bot.say(channel, "I am not the Droid you're looking for! Move Along Move Along");
+// Reply to highlights in Channels.
+bot.on("message", function(sender, channel, message) {
+  msgArray = message.split(" ");
+  console.log(msgArray);
+  if (msgArray[0] == config.nick + ":") {
+    bot.message(channel, sender.nick + ": " + "I am not the Droid you're looking for! Move Along Move Along.");
   };
 });
 
-//Git Webhook for Irc Post
-var gith = require('gith').create( 9001 );
+// Listen for any message, PM said user when he posts
+bot.on("pm", function(sender, message) {
+  bot.message(sender.nick, "I am a Bot and my Brains Fell Out");
+});
 
+// Start About Command
+bot.on('message', function (sender, channel, message) {
+  if (message == config.trigger + "about") {
+    var cpus = os.cpus();
+    var temperature = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");
+    //for(var i = 0, len = cpus.length; i < len; i++) {
+      var cpu = cpus[0];
+
+      bot.message(channel, sender.nick + " I Have Pm'd You My About Details.");
+      bot.message(sender.nick, "Bot Name: " + config.nick);
+      bot.message(sender.nick, "Operating System: " + os.release());
+      bot.message(sender.nick, "Architecture: " + os.arch());
+      bot.message(sender.nick, "Machine Type: " + config.machine);
+      bot.message(sender.nick, "CPU: " + cpu.model);
+      bot.message(sender.nick, "CPU Speed: " + cpu.speed + "MHz");
+      bot.message(sender.nick, "CPU Temp: " + (temperature/1000).toPrecision(3) + "°C");
+      bot.message(sender.nick, "Total Mem: " + (os.totalmem()/1024/1024/1024).toPrecision(4) + "GB Free Mem: " + (os.freemem()/1024/1024/1024).toPrecision(4) + "GB");
+      bot.message(sender.nick, "Uptime: " + (os.uptime()/60/1000).toPrecision(3) + " Days");
+      bot.message(sender.nick, "Maintainer: " + config.maintainer);
+    //};
+  };
+});
+// End About
+
+//Git Webhook for Irc Post
 gith({}).on( 'all', function( payload ) {
 
 
@@ -91,10 +178,10 @@ gith({}).on( 'all', function( payload ) {
   };
 
   var addedFiles = "";
-   
+
   if(!gitdata.added.length < 1) {
     addedFiles += "Files Added: ";
-        
+
     for(var i = 0; i < gitdata.added.length; i++) {
       if(gitdata.added.length == 1 || i == 0) {
         addedFiles += gitdata.added[0];
@@ -105,10 +192,10 @@ gith({}).on( 'all', function( payload ) {
   }
 
   var moddedFiles = "";
-   
+
   if(!gitdata.modded.length < 1) {
     moddedFiles += "Files Modified: ";
-      
+
   for(var i = 0; i < gitdata.modded.length; i++) {
       if(gitdata.modded.length == 1 || i == 0) {
         moddedFiles += gitdata.modded[0];
@@ -119,10 +206,10 @@ gith({}).on( 'all', function( payload ) {
   }
 
   var deletedFiles = "";
-   
+
   if(!gitdata.deleted.length < 1) {
   deletedFiles += "Files Deleted: ";
-      
+
   for(var i = 0; i < gitdata.deleted.length; i++) {
       if(gitdata.deleted.length == 1 || i == 0) {
         deletedFiles += gitdata.deleted[0];
@@ -132,129 +219,17 @@ gith({}).on( 'all', function( payload ) {
     }
   }
 
-  bot.say( '#thesetkehproject', gitdata.update + gitdata.comment );
-  bot.say( '#thesetkehproject', 'Repo: ' + gitdata.title );
-  bot.say( '#thesetkehproject', addedFiles );
-  bot.say( '#thesetkehproject', moddedFiles );
-  bot.say( '#thesetkehproject', deletedFiles );
-  bot.say( '#thesetkehproject', 'Pusher: ' + gitdata.pusher );
-  bot.say( '#thesetkehproject', 'Clone this Repo: ' + 'git clone ' + gitdata.url + '.git');
-  bot.say( '#thesetkehproject', gitdata.head );
+  bot.message( '#thesetkehproject', gitdata.update + gitdata.comment );
+  bot.message( '#thesetkehproject', 'Repo: ' + gitdata.title );
+  bot.message( '#thesetkehproject', addedFiles );
+  bot.message( '#thesetkehproject', moddedFiles );
+  bot.message( '#thesetkehproject', deletedFiles );
+  bot.message( '#thesetkehproject', 'Pusher: ' + gitdata.pusher );
+  bot.message( '#thesetkehproject', 'Clone this Repo: ' + 'git clone ' + gitdata.url + '.git');
+  bot.message( '#thesetkehproject', gitdata.head );
 });
 // End Github Webhook.
 
-// Start URL Snarfing
-bot.addListener('message', function (from, to, message) {
-  var msgArray = message.split(" ");
-  var Chan = to
-    for (var i = 0; i < msgArray.length; i++) {
-
-      if (msgArray[i].match(".jpg") || msgArray[i].match(".png") || msgArray[i].match("imgur") || msgArray[i].match(".gif")) {
-        console.log(Chan, "Ignoring Image");
-      } else {
-
-        if (msgArray[i].match("http://") || msgArray[i].match("https://")) {
-          var url = msgArray[i];
-
-          request({uri: url}, function(err, response, body) {
-
-            if (!err && response.statusCode) {
-              var regex_match = body.match(/<title>(.+?)<\/title>/)[1];
-              bot.say(Chan,  "[ Page Title: " + url + " " + regex_match + " ]");
-            } else {
-              bot.say(Chan, url + " Could not be found");
-              bot.say(Chan, err.message);
-            }
-
-          });
-        };
-      };
-    }
-});
-// End URL Snarfing
-
-// Start About Command
-bot.addListener('message', function (from, to, message) {
-  if (message == trigger + "about") {
-    var cpus = os.cpus();
-    var temperature = fs.readFileSync("/sys/class/thermal/thermal_zone0/temp");
-    //for(var i = 0, len = cpus.length; i < len; i++) {
-      var cpu = cpus[0];
-
-      bot.say(to, from + " I Have Pm'd You My About Details");
-      bot.say(from, "Bot Name: " + config.botName);
-      bot.say(from, "Operating System: " + os.release());
-      bot.say(from, "Architecture: " + os.arch());
-      bot.say(from, "Machine Type: " + machine);
-      bot.say(from, "CPU: " + cpu.model);
-      bot.say(from, "CPU Speed: " + cpu.speed + "MHz");
-      bot.say(from, "CPU Temp: " + (temperature/1000).toPrecision(3) + "°C");
-      bot.say(from, "Total Mem: " + os.totalmem() + " Free Mem: " + os.freemem());
-      bot.say(from, "Uptime: " + (os.uptime()/60/1000).toPrecision(3) + " Days");
-      bot.say(from, "Maintainer: " + maintainer);
-    //};
-  };
-});
-// End About
-
-// Start Github Search
-bot.addListener('message', function (from, to, message) {
-  var isquoted = message.split(" ");
-
-  for (var i = 0; i < isquoted.length; i++) {
-    if (isquoted[i].match(trigger + "github")) {
-
-      if (isquoted[1].match("\"")) {
-        var msgArray = message.split("\"");
-        var url = "https://api.github.com/search/repositories?q=";
-        var urlescape = "%20";
-
-        for (var i = 0; i < msgArray.length; i++) {
-
-          if (msgArray[i].match(trigger + "github")) {
-            var msgArray2 = msgArray[1].split(" ");
-
-            if (msgArray2.length == 1) {
-
-              request({uri: url + msgArray[1] + "&sort=stars&order=desc", headers: {'User-Agent': 'Kernel_Segfault'}},
-                function(err, response, body) {
-
-                if (!err && response.statusCode) {
-                  var results = JSON.parse(body);
-                  var returnurl = "https://github.com/" + results.items[0].full_name + "/";
-
-                  if (results.items.length == 0){
-                    bot.say(to, "Search Returned No Results");
-                  } else {
-                    bot.say(to, "Top Github Search Result: " + returnurl);
-                  }
-
-                };
-
-              });
-
-            } else if (msgArray2.length == 2) {
-              request({uri: url + msgArray2[0] + urlescape + msgArray2[1] + "&sort=stars&order=desc", headers: {'User-Agent': 'Kernel_Segfault'}},
-                function(err, response, body) {
-
-                  if (!err && response.statusCode) {
-                    var results = JSON.parse(body);
-                    var returnurl = "https://github.com/" + results.items[0].full_name + "/";
-
-                    if (results.items.length == 0){
-                      bot.say(to, "Search Returned No Results");
-                    } else {
-                      bot.say(to, "Top Github Search Result: " + returnurl);
-                    }
-                  }
-                }
-            )};
-          };
-        };
-      } else {
-        bot.say(to, "Search Strings Must be Encased in Quotes")
-      }
-    }
-  }
-});
-// End Github Search
+process.on('uncaughtException', function (err) {
+  console.log("ERROR: " + err);
+})
